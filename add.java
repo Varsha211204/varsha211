369518afee2c4d47b806471e8ada122b49ab1e99
Varsha211204 <varshavenugopal2112@gmail.com>
2025-03-17 15:30:25 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,6 +1,8 @@
 public class add{
   public static void main(String args[]){
-    int r=3,area=0;
+    Scanner sc=new Scanner(System.in);
+    int r=sc.nextInt();
+    int area=0;
     area=3.14*3*3;
     System.out.println("Area:" +area);
   }
